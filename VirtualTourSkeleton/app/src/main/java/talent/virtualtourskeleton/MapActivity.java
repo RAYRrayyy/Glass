3688b8e0a8dabb914a94d5c2bfe2929de2e8fdc4
Talent Branjerdporn <talentbran@hotmail.com>
2017-05-26 09:50:27 +1000

    Added application skeleton

---
+++ VirtualTourSkeleton/app/src/main/java/talent/virtualtourskeleton/MapActivity.java
@@ -0,0 +1,16 @@
+package talent.virtualtourskeleton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MapActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
+        getLayoutInflater().inflate(R.layout.content_map, contentFrameLayout);
+    }
+}
