a1411bdbfd3e3d1423b59972f1f351acd1c5b20e
RAYRrayyy <ray21@live.com.au>
2017-06-02 11:07:35 +1000

    Added the feedback server component version 1

---
+++ gsheetAsDb/app/src/main/java/androidlabs/gsheets2/util/Keys.java
@@ -0,0 +1,11 @@
+package androidlabs.gsheets2.util;
+
+
+public class Keys {
+
+    public static final String KEY_CONTACTS = "Sheet1";
+    public static final String KEY_NAME = "Category";
+    public static final String KEY_COUNTRY = "Location";
+  //  public static final String KEY_IMAGE = "image";
+
+}
