63f981285f9a1988e5ee111095147e74aff64fa9
RAYRrayyy <ray21@live.com.au>
2017-06-07 13:46:12 +1000

    Updated feedback app and python script

---
+++ ML/feedback.java
@@ -0,0 +1,258 @@
+package com.example.raysi.feedback;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    Button like, dislike;
+    TextView recommendation;
+    String user, location, preference;
+    Spinner locationSelect;
+    EditText userSelect;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        like = (Button)findViewById(R.id.like);
+        dislike = (Button)findViewById(R.id.dislike);
+
+        recommendation = (TextView)findViewById(R.id.recommendation); //Output/Next location recommendation
+
+        /*Preset using these, in practice these are based on actual device id and gps/barcode location*/
+        locationSelect = (Spinner) findViewById(R.id.locationSelect);
+        locationSelect.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        userSelect = (EditText)findViewById(R.id.userSelect);
+
+
+        /*When the Like Button is Hit*/
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                location = String.valueOf(locationSelect.getSelectedItem());
+                user = userSelect.getText().toString();
+                preference = "1"; //Like
+                new SendRequest().execute();
+            }
+
+        }   );
+
+        /*When the Dislike Button is Hit*/
+        dislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                location = String.valueOf(locationSelect.getSelectedItem());
+                user = userSelect.getText().toString();
+                preference = "-1"; //Dislike
+                new SendRequest().execute();
+            }
+
+        }   );
+
+    }
+    private void delay(int Seconds){
+        long Time = 0;
+        Time = System.currentTimeMillis();
+        while(System.currentTimeMillis() < Time+(Seconds*1000));
+    }
+    public class SendRequest extends AsyncTask<String, Void, String> {
+        protected void onPreExecute(){}
+        protected String doInBackground(String... arg0) {
+            try{
+                //Change your web app deployed URL or or u can use this for attributes (name, country)
+                URL url = new URL("https://script.google.com/macros/s/AKfycbya-jg7flbxyY9xEnG91Nv4_wtNx_oob9RwwRHdg2WJoUX9Eps/exec");
+                JSONObject postDataParams = new JSONObject();
+                //int i;
+                //for(i=1;i<=70;i++)
+                //    String usn = Integer.toString(i);
+                String id= "13ordFQXWTch9kvvulG08rtA76W-nKKcHwk887UiY59Q";
+                postDataParams.put("X","0");
+                postDataParams.put("user",user);
+                postDataParams.put("location",location);
+                postDataParams.put("preference",preference);
+                postDataParams.put("id",id);
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getApplicationContext(), result,
+            //        Toast.LENGTH_LONG).show();
+            recommendation.setText(result);
+            delay(3);
+            new GetRecommendationRequest().execute();
+        }
+    }
+
+    public class GetRecommendationRequest extends AsyncTask<String, Void, String> {
+        protected void onPreExecute(){}
+        protected String doInBackground(String... arg0) {
+            try{
+                //Change your web app deployed URL or or u can use this for attributes (name, country)
+                URL url = new URL("https://script.google.com/macros/s/AKfycbya-jg7flbxyY9xEnG91Nv4_wtNx_oob9RwwRHdg2WJoUX9Eps/exec");
+                JSONObject postDataParams = new JSONObject();
+                //int i;
+                //for(i=1;i<=70;i++)
+                //    String usn = Integer.toString(i);
+                String id= "13ordFQXWTch9kvvulG08rtA76W-nKKcHwk887UiY59Q";
+                postDataParams.put("X","1");
+                postDataParams.put("user",user);
+                postDataParams.put("id",id);
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getApplicationContext(), result,
+            //        Toast.LENGTH_LONG).show();
+            recommendation.setText(result);
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+
+
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            Toast.makeText(parent.getContext(),
+                    "Current Location Set to : " + parent.getItemAtPosition(pos).toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+    }
+}
--- ML/recommendation.py
+++ ML/recommendation.py
@@ -4,6 +4,33 @@ import gspread
 from numpy import *
 from oauth2client.service_account import ServiceAccountCredentials
 
+#RECOMMENDATIONS#
+
+#Class for learning models, different for each location#
+class location_learner:
+    def __init__(self, location_number):
+        self.location_number = location_number
+
+    def train_model(self, data):
+        self.data = data
+
+        #TRAINING FOR LOCATION 1 - Chancellor's Place#
+        if self.location_number == '1':
+            #Possible destinations: 13,18,6,4#
+            self.temp = copy(data[:,[4,6,13,18,1]])
+            #Replace zeros with -1#
+            self.train_data = copy(self.temp)
+            self.train_data[self.temp=="0"] = "-1"
+            print(self.train_data)
+
+
+
+    #Given data (user's past locations) and current location#
+    def determine_location(self, data,location_number):
+        print('stuff')
+
+#MAIN LOOP AND GOOGLE SHEET COMMUNICATION#
+
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
 scope = ['https://spreadsheets.google.com/feeds']
@@ -13,13 +40,31 @@ def update_recommendation(temp, num, worksheet):
     #print("data row",temp)
     #print("row number",num)
     #Update Recommendation#
-    worksheet.update_cell(num+2,30,"update")
+    worksheet.update_cell(num+2,31,"update")
     #clear the request#
-    worksheet.update_cell(num+2,29,'0')
-    worksheet.update_acell('AE1','0')
+    worksheet.update_cell(num+2,30,'0')
+    worksheet.update_acell('AF1','0')
 
-#def trainModel(temp)
+def init_models(worksheet):
+    temp = get_data(worksheet)
+    #Removes names and unneccessary column#
+    temp = delete(temp,(0),axis=1)
+    temp = delete(temp,(-1),axis=1)
 
+    #Create Class Instances for each location#
+    model_one = location_learner("1")
+    model_one.train_model(temp)
+
+def get_data(worksheet):
+    #`Retrieves the data from the database#
+    temp = asarray(worksheet.get_all_values())
+
+    #Removes headings and server communication columns#
+    temp = delete(temp,(0), axis=0)
+    temp = delete(temp,(-1),axis=1)
+    temp = delete(temp,(-1),axis=1)
+    temp = delete(temp,(-2),axis=1)
+    return temp
 
 def main():
     """ Retrieves data from the google sheet and checks if a request has been made
@@ -28,20 +73,14 @@ def main():
     credentials = ServiceAccountCredentials.from_json_keyfile_name('My Project-e401bb1cb11e.json', scope)
     gs = gspread.authorize(credentials)
     worksheet = gs.open("CSSE4011Dataset").sheet1  
-    i = 0
-    #TRAIN THE ML MODEL#
-    #`Retrieves the data from the database#
-    #temp = asarray(worksheet.get_all_values())
-    #temp = delete(temp,(0), axis=0)
-    #predictionModel = trainModel(temp)
+    count = 0
+
+    #Initiate ML Models#
+    init_models(worksheet)
 
     while True:       
-        if (worksheet.acell('AE1').value == '1'):
-            #`Retrieves the data from the database#
-            temp = asarray(worksheet.get_all_values())
-            temp = delete(temp,(0), axis=0)
-            temp = delete(temp,(-1), axis=1)
-            temp = delete(temp,(-1),axis=1)
+        if (worksheet.acell('AF1').value == '1'):
+            temp = get_data(worksheet)
             #Gets which users need updating#
             i = 0
             request_log = []
@@ -55,8 +94,8 @@ def main():
             #time.sleep() #sleeps so a check is made every ~15 seconds
 
         #Keeps Track of iterations/check if server still alive
-        print("Iteration = ", i)
-        i+=1
+        print("Iteration = ", count)
+        count+=1
 
 if __name__ == '__main__':
     main()
